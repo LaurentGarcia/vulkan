20ad5ba3ac92b1ca5b0bdb50b6ed75035cf4a2f6
laurentgarcia <laurent.garcia.carro@gmail.com>
2016-11-07 10:28:47 -0600

    starting the repo

--- main.cpp
+++ main.cpp
@@ -2,9 +2,29 @@
  * main.cpp
  *
  *  Created on: Nov 7, 2016
- *      Author: lcarro
+ *      Author: lauren garcia, www.loganvfx.com
+ *      Vulkan APP Resource and series of classes to help using Vulkan API
+ *      Based in the tutorials online at: https://vulkan-tutorial.com
+ *
+ *      To Download
  */
 
 
+#ifdef __linux__
+//linux code goes here
+	#define GLFW_INCLUDE_VULKAN
+    #include <stdio.h>
+    #include <iostream>
+	#include <glfw3.h>
+	#include <stdexcept>
+	#include <functional>
+	#include <vector>
 
+#endif
 
+const int SCREEN_WIDTH = 512;
+const int SCREEN_HEIGHT = 512;
+
+int main(void){
+	return 0;
+}
